eacf1d45328137ebc6b3f809d50eaa8a6cf912a1
Bhavya <singhpappu966@gmail.com>
2021-08-12 14:31:08 +0530

    Added Backend

---
+++ database.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+
+
+const logInSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+// mongoose model
+
+const logIn = mongoose.model("Detail", logInSchema);  // (table Name, schema)
+
+// new document
+const data1 = new logIn({
+    username: "Bhavya",
+    password: "bhavya"
+})
+
+
+
+data1.save();
+
+// module.exports = mongoose.model("Detail", logInSchema);
+
+module.exports = logIn;
+
